a5f535a18ef141fb4e83ad909b9c909652c642fe
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-31 09:56:09 -0600

    add sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  
+  resources :users, only: [:new, :create]
+  resources :links, only: [:index]
 end
---
+++ spec/features/guest_can_sign_up_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature "Guest can sign up" do
+  scenario "guest provides valid information" do
+    visit root_path
+    click_link "Sign Up"
+    
+    expect(current_path).to eq new_user_path
+    fill_in :user_email, with: "roberta@example.com"
+    fill_in :user_password, with: "password"
+    fill_in :user_password_confirmation, with: "password"
+    click_button "Sign Up"
+
+    expect(current_path).to eq links_path
+  end
+end
