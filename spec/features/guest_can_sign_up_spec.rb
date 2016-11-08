f2388cf1f429956541a010b38dd08baa2d8e8d80
Roberta Doyle <ro.scalabrin@gmail.com>
2016-11-08 09:47:44 -0700

    add tests for sign in and sign up sad paths

--- spec/features/guest_can_sign_up_spec.rb
+++ spec/features/guest_can_sign_up_spec.rb
@@ -13,4 +13,18 @@ feature "Guest can sign up" do
 
     expect(current_path).to eq links_path
   end
+  
+  scenario "guest provides invalid information" do
+    visit root_path
+    click_link "Sign Up"
+    
+    expect(current_path).to eq new_user_path
+    fill_in :user_email, with: "roberta@example.com"
+    fill_in :user_password, with: "password"
+    fill_in :user_password_confirmation, with: "pass"
+    click_button "Sign Up"
+
+    expect(current_path).to eq users_path
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
 end
